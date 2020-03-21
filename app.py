39011e2d3a5c0ec595efe06316813039bc2b6670
irinatu17 <iratushina17@gmail.com>
2020-03-21 14:41:59 +0000

    Changed a bug in Procfile

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=(os.environ.get('PORT')),
             debug=True)
