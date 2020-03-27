cb91868a755ecb51ddfcb7550e339a4091719ff0
irinatu17 <iratushina17@gmail.com>
2020-03-27 13:38:19 +0000

    Creating environment variables

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=(os.environ.get('PORT')),
+            port=int(os.environ.get('PORT')),
             debug=True)
